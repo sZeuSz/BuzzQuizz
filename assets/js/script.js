303dc0d5e885d85a8ab2cf59aa4c71d38f1089a7
AbellasJuan <juanvictoraa@gmail.com>
2021-08-13 19:39:00 -0300

    tela de exibir Quizzes done

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,19 +9,14 @@ function buscarQuizzes (){
 }
 
 function renderizarQuizzes(resposta){
-    console.log(resposta);
+    // console.log(resposta);
 
     for (let i=0; i<resposta.data.length; i++){
-
     let title = resposta.data[i].title;
-    let image = resposta.data[i].image;
-
-    console.log(resposta.data[i].title)
-    console.log(resposta.data[i].image)
-    
+    let image = resposta.data[i].image;   
     let divDosQuizzes = document.querySelector(".todos-os-quizzes");
     divDosQuizzes.innerHTML+=   
-            `<div class="box">
+            `<div onclick="abrirQuizz(this)" class="box" id="${i}">
                 <div class="background-linear"></div>
                 <img src="${image}">
                 <p>${title}</p>
@@ -29,7 +24,20 @@ function renderizarQuizzes(resposta){
     }    
 }
 
+//funçao pra mostrar quizzes:
+// function abrirQuizz(element){
+//     console.log(element);
+
+//     const IDdoQuizz = element.id;
+//     const quizzIndividual = listaIdDosQuizzes[IDdoQuizz];
+//     const quizzImage = quizzIndividual.image;
+//     const quizzTitulo = quizzIndividual.title;
+
+
 //Fim Juan
+
+
+
 
 
 //Inicio Roseno
